4e44143a36f5618b8c7d06e0b08cc4b7632ce8b6
MTaylorfullStack <mtaylor@codingdojo.com>
2020-07-07 20:12:39 -0500

    first_session

---
+++ week_one/fun.py
@@ -0,0 +1,83 @@
+## Data Types
+
+#number
+num = 10
+# +, -, *, /, %
+
+
+#string
+name = "Matt"
+
+print(num)
+print(name)
+
+#boolean
+result = True
+
+#lists
+students = ["Amy", "Thomas", "Nathalie", "Saneeta"]
+
+# {
+#     0:"Amy",
+#     1:"Thomas"
+# }
+
+print(len(students))
+
+students.append("Matt")
+
+students.pop()
+
+print(students)
+
+print(students[1])
+
+#dictionaries
+
+bad_guy = {
+    'health': 500,
+    'name': "Ka'Thar",
+    'attack': 'Mace Smash'
+}
+# name = 'attack'
+
+print(bad_guy['name'])
+
+bad_guy["attack"] = 'Mace Super Smash'
+
+print(bad_guy['attack'])
+
+#loops/conditionals
+
+students = ["Amy", "Thomas", "Nathalie", "Saneeta"]
+
+post = {
+    'poster': "Odion",
+    "content": "omg this python class won't end!!",
+    'post_date': '07/07/20'
+}
+
+count = 0
+
+for whatever in post:
+    count+=1
+    print(f"Object attr: {whatever}, Object value: {post[whatever]}")
+    print(count)
+
+# for(let i =0; i<250; i++){
+# }
+
+for i in range(len(students)):
+    print(f"This is the index: {i}, this is the value: {students[i]}")
+
+def print_name_return_ten(name):
+    print(name)
+    if name == "Ka'Thar":
+        return "You have run into the boss"
+    elif name == "Michael":
+        return "Oh no... run"
+    else:
+        return 10
+
+
+print(print_name_return_ten("Kyle"))
---
+++ week_one/session_one.md
@@ -0,0 +1,25 @@
+#### Learning Objectives
+-   Course Overview / Objectives
+-   Why Python? What is Python?
+-   Python syntax / best practices
+
+## Course Overview
+-   Week by week outline
+-   Goals / objectives
+    -   Assignment expectations
+    -   Exam expectations
+
+## Why Python?
+-   Python installation
+    -   What is pip (package manager)?
+    -   What is a virtual environment?
+-   The Zen of Python
+-   Industry popularity
+
+## Syntax / Best Practices
+-   Casing and spacing
+-   Variables, Data Types, Conditionals, Loops and Functions
+-   Solving algorithms with Python
+    -   Large Number - Add odd integers from 0 to 500,000, and print the final sum.
+    -   First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
+    -   Ultimate Analysis - Create a function that takes a list and returns a dictionary that has the sumTotal, average, minimum, maximum and length of the list.
