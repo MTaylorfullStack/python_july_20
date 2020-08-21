506253e55c25bc3fb9bf7fc2edb4e0e3353fce5a
MTaylorfullStack <mtaylor@codingdojo.com>
2020-08-21 08:47:20 -0500

    groups

---
+++ week_seven/groups_exam/group_pp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('main', views.main),
+    # registration paths
+    path('register', views.register),
+    path('login', views.login),
+    # groups paths
+    path('groups', views.groups),
+    path('create_org', views.create_org),
+    path('groups/<int:id>', views.one_group),
+    path('join/<int:id>', views.join_org),
+    path('leave/<int:id>', views.leave_org)
+]
